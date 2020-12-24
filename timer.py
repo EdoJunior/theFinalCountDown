0af4675ca158b55a2c6bdc0f4dbc8edb8140f35e
pocus <junioredouard91@gmail.com>
2020-12-25 00:26:23 +0100

    coded stuff my guy

--- timer.py
+++ timer.py
@@ -1,8 +1,24 @@
 # This project will be about a timer counting down from a set amount of time and the person is required to enter a password
 # before the timer reaches 0.00 or the timer will reset until correct password is entered.
 
+import time
 
 
+def countDown(arg):
+    t = 20
+    stat = True
+    while stat:
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(timer, end="\r")
+            time.sleep(1)
+            t -= 1
 
+    print("Yezzir buddy")
+
+
+answer = input("Enter the Answer NOW!!!")
+
+countDown(answer)
 
 
