6b5c8e27e23320e52f973b08211f2938d157b7a9
pocus <junioredouard91@gmail.com>
2020-12-24 21:37:04 +0100

    checking

--- timer.py
+++ timer.py
@@ -1,1 +1,14 @@
-print("hello");
+# This project will be about a timer counting down from a set amount of time and the person is required to enter a password
+# before the timer reaches 0.00 or the timer will reset until correct password is entered.
+
+import time
+print("please enter password!c")
+
+t = input("enter password")
+
+print(t)
+
+
+
+
+
